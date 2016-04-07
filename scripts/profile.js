30cd5e0497fa407fcf0384162a1c8aa2647f0249
Luke Bell <luke.bell66@hotmail.co.uk>
2016-04-07 09:43:46 +0200

    edited images

---
+++ scripts/profile.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function(){
+
+	//set height of instaWidget
+	var middleHeight = $("#middlePanel").height();	
+	$("#rightPanel").height(middleHeight);
+	$("#instaWidget").height(middleHeight-50);
+
+});
